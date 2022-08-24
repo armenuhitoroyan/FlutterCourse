603e3a8cbff35c878665b6fc1b2bbb4b7d48d385
Armenouhi <40177133+Armenouhi@users.noreply.github.com>
2022-08-24 23:47:43 +0400

    ListView, ListTile, Image

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/widgets/home_page_widget.dart';
+
+void main() {
+  runApp(const HomePageWidget());
+}
+
+
+
+
+
+
---
+++ lib/widgets/home_page_widget.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/widgets/tab_bar_widget.dart';
+
+class HomePageWidget extends StatelessWidget {
+  const HomePageWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.camera_alt_rounded)),
+                Tab(text: 'CHATS'),
+                Tab(text: 'STATUS'),
+                Tab(text: 'CALLS'),
+              ],
+            ),
+            title: const Text('WhatsApp'),
+          ),
+          body: ListView(
+            padding: const EdgeInsets.all(1),
+            children: const <Widget>[
+              ListTile(
+                  title: Text(
+                      "Battery Full"
+                  ),
+                  subtitle: Text(
+                      "The battery is full."
+                  ),
+                  leading:  CircleAvatar(
+                    backgroundImage: AssetImage(
+                        "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                    ), // no matter how big it is, it won't overflow
+                  ),
+                  trailing: Text('2016'),
+              ),
+              ListTile(
+                title: Text("Anchor"),
+                subtitle: Text("Lower the anchor."),
+                leading:CircleAvatar(
+                  backgroundImage: AssetImage(
+                    "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                  ), // no matter how big it is, it won't overflow
+                ),
+                trailing: Text('2013'),
+              ),
+              ListTile(
+                  title: Text("Alarm"),
+                  subtitle: Text("This is the time."),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                    ), // no matter how big it is, it won't overflow
+                  ),
+                  trailing: Text('2018')
+              ),
+              ListTile(
+                  title: Text("Ballot"),
+                  subtitle: Text("Cast your vote."),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                    ), // no matter how big it is, it won't overflow
+                  ),
+                  trailing: Text('2017')
+              ),
+              ListTile(
+                  title: Text("Ballot"),
+                  subtitle: Text("Cast your vote."),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                    ), // no matter how big it is, it won't overflow
+                  ),
+                  trailing: Text('2015')
+              ),
+              ListTile(
+                  title: Text("Ballot"),
+                  subtitle: Text("Cast your vote."),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                    ), // no matter how big it is, it won't overflow
+                  ),
+                  trailing: Text('2013')
+              ),
+              ListTile(
+                  title: Text("Ballot"),
+                  subtitle: Text("Cast your vote."),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                    ), // no matter how big it is, it won't overflow
+                  ),
+                  trailing: Text('2014')
+              ),
+              ListTile(
+                  title: Text("Ballot"),
+                  subtitle: Text("Cast your vote."),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                    ), // no matter how big it is, it won't overflow
+                  ),
+                  trailing: Icon(Icons.star)
+              ),
+              ListTile(
+                  title: Text("Ballot"),
+                  subtitle: Text("Cast your vote."),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                    ), // no matter how big it is, it won't overflow
+                  ),
+                  trailing: Icon(Icons.star)
+              ),
+              ListTile(
+                  title: Text("Ballot"),
+                  subtitle: Text("Cast your vote."),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                    ), // no matter how big it is, it won't overflow
+                  ),
+                  trailing: Text('2013')
+              ),
+              ListTile(
+                  title: Text("Ballot"),
+                  subtitle: Text("Cast your vote."),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                    ), // no matter how big it is, it won't overflow
+                  ),
+                  trailing: Text('2007')
+              ),
+              ListTile(
+                  title: Text("Ballot"),
+                  subtitle: Text("Cast your vote."),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                    ), // no matter how big it is, it won't overflow
+                  ),
+                  trailing: Icon(Icons.star)
+              ),
+              ListTile(
+                  title: Text("Ballot"),
+                  subtitle: Text("Cast your vote."),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                    ), // no matter how big it is, it won't overflow
+                  ),
+                  trailing: Text('2013')
+              ),
+              ListTile(
+                  title: Text("Ballot"),
+                  subtitle: Text("Cast your vote."),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                    ), // no matter how big it is, it won't overflow
+                  ),
+                  trailing: Text('2013')
+              ),
+              ListTile(
+                  title: Text("Ballot"),
+                  subtitle: Text("Cast your vote."),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                    ), // no matter how big it is, it won't overflow
+                  ),
+                  trailing: Text('2004')
+              ),
+              ListTile(
+                  title: Text("Ballot"),
+                  subtitle: Text("Cast your vote."),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "https://image.shutterstock.com/image-vector/business-user-icon-users-group-260nw-412967089.jpg",
+                    ), // no matter how big it is, it won't overflow
+                  ),
+                  trailing: Text('2013')
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
