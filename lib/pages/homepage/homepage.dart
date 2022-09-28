60620c0aca0753c8adfae8d82323de0051c258a1
Armenouhi <40177133+Armenouhi@users.noreply.github.com>
2022-09-28 21:59:18 +0400

    Add list of data

--- lib/pages/homepage/homepage.dart
+++ lib/pages/homepage/homepage.dart
@@ -16,9 +16,48 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(
-        child: const Text(
-          'Home',
+      body: SafeArea(
+        // ignore: avoid_unnecessary_containers
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: ListView.builder(
+              itemCount: 3,
+              itemBuilder: (context, index) => 
+               Padding(
+                padding: const EdgeInsets.only(top: 10, bottom: 10),
+                 child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 56,
+                          backgroundColor: Colors.cyan,
+                          child: Container(
+                            height: double.infinity,
+                            alignment: Alignment.center,
+                            child: ClipOval(
+                              child: Image.network(
+                                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_EZwam8kElu8CYpDDTDLHcp7upEP8MtUlPA&usqp=CAU',
+                              ),
+                            ),
+                          ),
+                        ),
+                        const Expanded(
+                          flex: 2,
+                          child: Center(
+                            child: Text(
+                              'Անուն Ազգանուն',
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                             ),
+               ),
+            ),
+          ),
         ),
       ),
     );
