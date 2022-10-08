4763054b520b86ee47a667ac25583d917953ca90
Armenouhi <40177133+Armenouhi@users.noreply.github.com>
2022-10-09 01:23:33 +0400

    change ui

--- lib/pages/homepage/leaderboard/leaderboard.dart
+++ lib/pages/homepage/leaderboard/leaderboard.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:exam_at/pages/homepage/leaderboard/leaderboard_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
