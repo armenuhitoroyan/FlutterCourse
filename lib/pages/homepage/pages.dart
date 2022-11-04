c70d477c5e1d3b052b8dc99c40ed0c9ab7fbf55a
Armenouhi <40177133+Armenouhi@users.noreply.github.com>
2022-11-04 17:07:59 +0400

    use visibility

--- lib/pages/homepage/pages.dart
+++ lib/pages/homepage/pages.dart
@@ -12,6 +12,8 @@ class Pages extends StatefulWidget {
 
 class _PagesState extends State<Pages> {
   List<Widget> pages = const [Games(), Score(), LeaderBoard(), ClimePrize()];
+
+  bool _isVisible = true;
   // ignore: prefer_final_fields
   PageController _controller = PageController(
     initialPage: 0,
@@ -34,36 +36,42 @@ class _PagesState extends State<Pages> {
           margin: const EdgeInsets.symmetric(),
           child: Stack(
             children: [
-              PageView(
-                controller: _controller,
-                children: pages,
-              ),
-              // PageView.builder(
-              //   itemCount: pages.length,
-              //   itemBuilder: (context, index) {
-              //     i = index;
-              //     return pages[index];
-
-              //   },
+              // PageView(
+              //   controller: _controller,
+              //   children: pages,
               // ),
+              PageView.builder(
+                itemCount: pages.length,
+                itemBuilder: (context, index) {
+                  i = index;
+                  print(i);
+                  if (i == pages.length - 1) {
+                    _isVisible = false; 
+                  }
+                  return pages[index];
+                },
+              ),
               Padding(
                 padding: const EdgeInsets.all(10.0),
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: SmoothPageIndicator(
-                    controller: _controller,
-                    count: pages.length,
-                    effect: ExpandingDotsEffect(
-                        activeDotColor: const Color.fromRGBO(204, 0, 1, 0.8),
-                        dotColor: Colors.red.shade100,
-                        dotWidth: 30,
-                        dotHeight: 30),
-                    onDotClicked: (index) {
-                      i = index;
-                      if (i == index) {
-                        print('$i');
-                      }
-                    },
+                child: Visibility(
+                  visible: _isVisible,
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: SmoothPageIndicator(
+                      controller: _controller,
+                      count: pages.length,
+                      effect: ExpandingDotsEffect(
+                          activeDotColor: const Color.fromRGBO(204, 0, 1, 0.8),
+                          dotColor: Colors.red.shade100,
+                          dotWidth: 30,
+                          dotHeight: 30),
+                      onDotClicked: (index) {
+                        i = index;
+                        if (i == index) {
+                          print('$i');
+                        }
+                      },
+                    ),
                   ),
                 ),
               )
