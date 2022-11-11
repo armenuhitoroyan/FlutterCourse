bc5ea542546a98ea5b3d50def91be533b993cf9b
Armenouhi <40177133+Armenouhi@users.noreply.github.com>
2022-11-11 11:56:17 +0400

    fix the bug of form group

--- lib/homepage/forms.dart
+++ lib/homepage/forms.dart
@@ -3,7 +3,7 @@ import 'package:reactive_forms/reactive_forms.dart';
 class FormsState {
   final form = FormArray([
     FormGroup({
-      'relationship': FormControl<String>(value: 'rel'),
+      'relationship': FormControl<String>(),
       'position': FormControl<String>(),
       'note': FormControl<String>(),
     })
--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -25,84 +25,85 @@ class HomePage extends StatelessWidget {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    ReactiveForm(
-                      formGroup: state.formArray!.controls.first as FormGroup,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
-                                children: [
-                                  Flexible(
-                                    flex: 1,
-                                    child: Padding(
-                                      padding: const EdgeInsets.only(left: 8.0),
-                                      child: Row(
-                                        children: const [
-                                          Text(
-                                            'RELATIONSHIPS',
-                                            style: TextStyle(
-                                              color: Color.fromRGBO(
-                                                  204, 0, 1, 0.8),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 8.0),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                Flexible(
+                                  flex: 1,
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left: 8.0),
                                     child: Row(
-                                      children: [
-                                        Container(
-                                          decoration: BoxDecoration(
-                                            color: Colors.white,
-                                            borderRadius:
-                                                BorderRadius.circular(10.0),
-                                            border: Border.all(
-                                              color: const Color.fromRGBO(
-                                                  204, 0, 1, 0.8),
-                                            ),
+                                      children: const [
+                                        Text(
+                                          'RELATIONSHIPS',
+                                          style: TextStyle(
+                                            color:
+                                                Color.fromRGBO(204, 0, 1, 0.8),
                                           ),
-                                          child: IconButton(
-                                            onPressed: () {
-                                              if (state.onClicked == true) {
-                                                state.formArray!.add(state
-                                                    .formArray!.controls.first);
-                                              }
-                                            },
-                                            style: const ButtonStyle(),
-                                            icon: const Icon(
-                                              Icons.add,
-                                              color: Color.fromRGBO(
-                                                  204, 0, 1, 0.8),
-                                            ),
-                                          ),
-                                        )
+                                        ),
                                       ],
                                     ),
                                   ),
-                                ],
-                              ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 8.0),
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                          border: Border.all(
+                                            color: const Color.fromRGBO(
+                                                204, 0, 1, 0.8),
+                                          ),
+                                        ),
+                                        child: IconButton(
+                                          onPressed: () {
+                                            // if (state.onClicked == true) {
+                                            state.formArray!.add(FormGroup({
+                                              'relationship':
+                                                  FormControl<String>(),
+                                              'position': FormControl<String>(),
+                                              'note': FormControl<String>(),
+                                            }));
+                                            // }
+                                          },
+                                          style: const ButtonStyle(),
+                                          icon: const Icon(
+                                            Icons.add,
+                                            color:
+                                                Color.fromRGBO(204, 0, 1, 0.8),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
                             ),
-                            ListView.builder(
-                              itemCount: state.formArray!.controls.length - 1,
-                              shrinkWrap: true,
-                              itemBuilder: (context, index) => Dismissible(
-                                key: Key(state.formArray!.controls.toString()),
-                                onDismissed: (direction) {
-                                  state.formArray!.removeAt(index);
-                                },
-                                child: _buildcontent(),
-                              ),
-                            )
-                          ],
-                        ),
+                          ),
+                          ListView.builder(
+                            itemCount: state.formArray!.controls.length - 1,
+                            shrinkWrap: true,
+                            itemBuilder: (context, index) => Dismissible(
+                              key: Key(state.formArray!.controls.toString()),
+                              onDismissed: (direction) {
+                                state.formArray!.removeAt(index);
+                              },
+                              child: _buildcontent(formArray.controls[index], state, index),
+                            ),
+                          )
+                        ],
                       ),
                     )
                   ],
@@ -116,7 +117,7 @@ class HomePage extends StatelessWidget {
   }
 }
 
-Widget _buildcontent() {
+Widget _buildcontent(formGroup, state, i) {
   List<DropdownMenuItem<dynamic>> list = <String>[
     'One',
     'Two',
@@ -133,111 +134,108 @@ Widget _buildcontent() {
     color: Colors.grey[100],
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(10.0),
-      // side: const BorderSide(
-      //   color: Colors.greenAccent,
-      // ),
+     
     ),
     child: Padding(
       padding: const EdgeInsets.all(10.0),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Flexible(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 20, left: 10),
-                  child: ReactiveDropdownField(
-                    formControlName: 'relationship',
-                    items: list,
-                    decoration: const InputDecoration(
-                      labelText: 'Relationship',
-                    ),
-                    // validationMessages: '',
-                  ),
-                ),
-              ),
-              Flexible(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20, right: 10),
-                  child: ReactiveTextField(
-                    formControlName: 'position',
-                    decoration: const InputDecoration(
-                      labelText: 'Position',
-                    ),
-                    textCapitalization: TextCapitalization.words,
-                    style: const TextStyle(
-                      backgroundColor: Colors.white,
-                    ),
-                    
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Flexible(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 10, right: 10),
-                  child: ReactiveTextField(
-                    formControlName: 'note',
-                    decoration: const InputDecoration(
-                      labelText: 'Note',
-                    ),
-                    textCapitalization: TextCapitalization.words,
-                    style: const TextStyle(
-                      backgroundColor: Colors.white,
+      child: ReactiveForm(
+        formGroup: state.formArray!.controls[i] as FormGroup,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 20, left: 10),
+                    child: ReactiveDropdownField(
+                      formControlName: 'relationship',
+                      items: list,
+                      decoration: const InputDecoration(
+                        labelText: 'Relationship',
+                      ),
+                      // validationMessages: '',
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Flexible(
-                flex: 1,
-                child: Row(
-                  children: [
-                    Row(
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            
-                          },
-                          icon: const Icon(
-                            Icons.close,
+                Flexible(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 10),
+                    child: ReactiveTextField(
+                      formControlName: 'position',
+                      decoration: const InputDecoration(
+                        labelText: 'Position',
+                      ),
+                      textCapitalization: TextCapitalization.words,
+                      style: const TextStyle(
+                        backgroundColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    child: ReactiveTextField(
+                      formControlName: 'note',
+                      decoration: const InputDecoration(
+                        labelText: 'Note',
+                      ),
+                      textCapitalization: TextCapitalization.words,
+                      style: const TextStyle(
+                        backgroundColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.close,
+                            ),
                           ),
-                        ),
-                        const Text('Cancel'),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.save_sharp,
+                          const Text('Cancel'),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.save_sharp,
+                            ),
                           ),
-                        ),
-                        const Text('Save'),
-                      ],
-                    )
-                  ],
+                          const Text('Save'),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.delete),
-              ),
-            ],
-          ),
-        ],
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.delete),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     ),
   );
 }
-
