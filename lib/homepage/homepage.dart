b971828d964e442445b36a112b414fc62387c21a
Armenouhi <40177133+Armenouhi@users.noreply.github.com>
2022-11-11 09:45:05 +0400

    the file changed

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note/homepage/bloc/note_bloc.dart';
 import 'package:note/homepage/forms.dart';
-import 'package:note/homepage/rforms.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
 FormsState formsState = FormsState();
@@ -99,7 +98,7 @@ class HomePage extends StatelessWidget {
                                 onDismissed: (direction) {
                                   state.formArray!.removeAt(index);
                                 },
-                                child: RForm(),
+                                child: _buildcontent(),
                               ),
                             )
                           ],
@@ -116,3 +115,129 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+Widget _buildcontent() {
+  List<DropdownMenuItem<dynamic>> list = <String>[
+    'One',
+    'Two',
+    'Free',
+    'Four',
+  ].map<DropdownMenuItem<String>>((String value) {
+    return DropdownMenuItem<String>(
+      value: value,
+      child: Text(value),
+    );
+  }).toList();
+
+  return Card(
+    color: Colors.grey[100],
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10.0),
+      // side: const BorderSide(
+      //   color: Colors.greenAccent,
+      // ),
+    ),
+    child: Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Flexible(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 10),
+                  child: ReactiveDropdownField(
+                    formControlName: 'relationship',
+                    items: list,
+                    decoration: const InputDecoration(
+                      labelText: 'Relationship',
+                    ),
+                    // validationMessages: '',
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 10),
+                  child: ReactiveTextField(
+                    formControlName: 'position',
+                    decoration: const InputDecoration(
+                      labelText: 'Position',
+                    ),
+                    textCapitalization: TextCapitalization.words,
+                    style: const TextStyle(
+                      backgroundColor: Colors.white,
+                    ),
+                    
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Flexible(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: ReactiveTextField(
+                    formControlName: 'note',
+                    decoration: const InputDecoration(
+                      labelText: 'Note',
+                    ),
+                    textCapitalization: TextCapitalization.words,
+                    style: const TextStyle(
+                      backgroundColor: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Flexible(
+                flex: 1,
+                child: Row(
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            
+                          },
+                          icon: const Icon(
+                            Icons.close,
+                          ),
+                        ),
+                        const Text('Cancel'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.save_sharp,
+                          ),
+                        ),
+                        const Text('Save'),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.delete),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
