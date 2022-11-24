e76ac03a61f94219f202723479d8c366143abb0a
Armenouhi <40177133+Armenouhi@users.noreply.github.com>
2022-11-24 17:48:42 +0400

    use the stream

--- lib/pages/automations/bloc/searching_bloc.dart
+++ lib/pages/automations/bloc/searching_bloc.dart
@@ -2,11 +2,12 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:rxdart/streams.dart';
 
 part 'searching_event.dart';
 part 'searching_state.dart';
 
-Timer? _debounce;
 
 class SearchingBloc extends Bloc<SearchingEvent, SearchingState> {
   late bool isLoad;
@@ -17,18 +18,20 @@ class SearchingBloc extends Bloc<SearchingEvent, SearchingState> {
   }
 
   _onSearchEvent(SearchedEvent event, Emitter<SearchingState> emit) async {
-    if (await _debounce?.isActive ?? false) _debounce!.cancel();
-    _debounce = Timer(const Duration(milliseconds: 0), () async {
-      if (event.value.isNotEmpty) {
-        isLoad = true;
-        query = event.value;
-        print('1 $isLoad');
-      } else {
-        isLoad = false;
-        query = '';
-        print('2 $isLoad');
-      }
-    });
+
+    Stream.fromIterable([])
+        .debounce((_) => TimerStream(true, Duration(seconds: 0)))
+        .listen(print); //
+
+    if (event.value.isNotEmpty) {
+      isLoad = true;
+      query = event.value;
+      print('1 $isLoad');
+    } else {
+      isLoad = false;
+      query = '';
+      print('2 $isLoad');
+    }
 
     emit(state.copyWith(isLoaded: await isLoad, data: await query));
     // print('4 ${state.isLoaded}');
