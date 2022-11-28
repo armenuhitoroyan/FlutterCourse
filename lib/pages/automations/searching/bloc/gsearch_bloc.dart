f6279a125a2d32eaf887f9085dd7f5186450ba5d
Armenouhi <40177133+Armenouhi@users.noreply.github.com>
2022-11-28 11:52:31 +0400

    it has been used try_cartch

--- lib/pages/automations/searching/bloc/gsearch_bloc.dart
+++ lib/pages/automations/searching/bloc/gsearch_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:rxdart/rxdart.dart';
@@ -6,7 +8,7 @@ import 'package:rxdart/transformers.dart';
 part 'gsearch_event.dart';
 part 'gsearch_state.dart';
 
-
+Timer? _debounce;
 
 class GsearchBloc extends Bloc<GsearchEvent, GsearchState> {
   late bool isLoaded;
@@ -21,14 +23,21 @@ class GsearchBloc extends Bloc<GsearchEvent, GsearchState> {
   }
 
   _onGSearchEvent(GsearchedEvent event, Emitter<GsearchState> emitter) {
-    if (event.searchedValue != '') {
-      data = event.searchedValue;
-      isLoaded = true;
-    } else {
-      data = '';
+    // if (_debounce?.isActive ?? false) _debounce!.cancel();
+    // _debounce = Timer(const Duration(milliseconds: 500), () {
+    try {
+      if (event.searchedValue != '') {
+        data = event.searchedValue;
+        isLoaded = true;
+      } else {
+        data = '';
+        isLoaded = false;
+      }
+    } on Exception catch (e) {
       isLoaded = false;
     }
 
     emit(state.copyWith(isLoaded: isLoaded, searchValue: data));
+    // });
   }
 }
