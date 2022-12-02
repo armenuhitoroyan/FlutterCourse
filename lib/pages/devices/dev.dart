8863c07dd2109e8950080540e1b71dbcd2bea7a1
Armenouhi <40177133+Armenouhi@users.noreply.github.com>
2022-12-02 10:07:38 +0400

    the page devices converted statefull

--- lib/pages/devices/dev.dart
+++ lib/pages/devices/dev.dart
@@ -18,6 +18,9 @@ class _DevicesStateState extends State<DevicesState> {
   ];
 
   String dropdownValue = 'See More 1';
+  double width = 150;
+  bool isTurned = false;
+  double percent = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +28,7 @@ class _DevicesStateState extends State<DevicesState> {
   }
 
   Widget _contentBuilder() {
+    double contWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -81,9 +85,59 @@ class _DevicesStateState extends State<DevicesState> {
                           color: RangerColors.white),
                       child: Column(
                         children: [
-                          Stack(children: [
-                            Container(),
-                            Container()
+                          Padding(
+                            padding: const EdgeInsets.only(left: 15.0, top: 20.0),
+                            child: Row(
+                              children: const [
+                                Text(
+                                  'All Devices',
+                                  style: TextStyle(
+                                      fontSize: 25, color: RangerColors.black),
+                                  textAlign: TextAlign.start,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Stack(children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 15, right: 15, top: 20),
+                              child: SizedBox(
+                                width: width > 0 ? width : 0,
+                                height: 70,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      gradient: LinearGradient(colors: [
+                                        RangerColors.white,
+                                        isTurned
+                                            ? RangerColors.yellowBtn
+                                            : RangerColors.greyBottomBar
+                                      ])),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 20, left: 15, right: 15),
+                              child: Container(
+                                width: double.infinity,
+                                height: 70,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  border: Border.all(
+                                      color: isTurned
+                                          ? RangerColors.yellowBtn
+                                          : RangerColors.greyBottomBar),
+                                ),
+                                child: GestureDetector(
+                                  onHorizontalDragStart: (details) => 0,
+                                  onHorizontalDragUpdate: (details) =>
+                                      _onDecideEnlightenment(contWidth,
+                                          details.globalPosition.dx, percent),
+                                ),
+                              ),
+                            )
                           ])
                         ],
                       ),
@@ -101,6 +155,24 @@ class _DevicesStateState extends State<DevicesState> {
   void _onSelectItem(String? value) {
     setState(() {
       dropdownValue = value!;
+    });
+  }
+
+  void _onDecideEnlightenment(
+      double contWidth, double? slideErWidth, double percent) {
+    setState(() {
+      if (slideErWidth! > 0) {
+        contWidth = contWidth - 30;
+        slideErWidth = (slideErWidth!);
+        print('s1 $slideErWidth');
+        percent = ((100 * slideErWidth!) / contWidth - 1).clamp(0, 100);
+        print('p1 ${percent.round()}');
+      } else if (slideErWidth! < 0) {
+        slideErWidth = 0;
+        percent = 0;
+        print('p2 0');
+        print('s2 $slideErWidth');
+      }
     });
   }
 }
